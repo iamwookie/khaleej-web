a6fe3452706c031fcd67afdf8241f693c50111e6
Bilal <bq1214@gmail.com>
2024-11-22 23:34:53 +0000

    chore: bootstrap

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+    <title>Khaleej vACC</title>
+    <meta name="description" content="Khaleej vACC is a virtual area control center for the Bahrain and Kuwait region on the VATSIM network.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</svelte:head>
+
+{@render children()}
