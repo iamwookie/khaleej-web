13586b4b0ce03f7d0372cf535f8396a6f9a52b35
Bilal <bq1214@gmail.com>
2024-11-23 23:39:48 +0000

    feat: add header

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { Plane } from 'lucide-svelte';
+</script>
+
+<div class="navbar bg-transparent">
+	<div class="flex-1">KhaleejVACC</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal gap-1 px-1">
+			<li><a class="hover:bg-accent hover:bg-opacity-50" href="/">Home</a></li>
+			<li>
+				<details>
+					<summary class="hover:bg-accent hover:bg-opacity-50"><Plane size={20} /> Pilots</summary>
+					<ul class="menu menu-vertical bg-accent bg-opacity-50">
+						<li><a class="hover:bg-accent" href="/">Home</a></li>
+					</ul>
+				</details>
+			</li>
+		</ul>
+	</div>
+</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,5 +7,35 @@ export default {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [require('daisyui')],
+
+	daisyui: {
+        base: false,
+		themes: [
+			{
+				khaleej: {
+					primary: '#003366', // khaleej blue
+					'primary-content': '#160016',
+					secondary: '#d8c89f', // khaleej tan
+					'secondary-content': '#160016',
+					accent: '#d8c89f', // khaleej tan
+					'accent-content': '#001616',
+					neutral: '#191e24', // light gray
+					'neutral-content': '#a6adbb',
+					'base-100': '#191e24', // light gray
+					'base-200': '#15191e', // gray
+					'base-300': '#000000', // black
+					'base-content': '#ffffff', // white
+ 					info: '#0000ff',
+					'info-content': '#c6dbff',
+					success: '#00ff00',
+					'success-content': '#001600',
+					warning: '#00ff00',
+					'warning-content': '#001600',
+					error: '#ff0000',
+					'error-content': '#160000'
+				}
+			}
+		]
+	}
 } satisfies Config;
