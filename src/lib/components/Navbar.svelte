22bbc149f5f183fd872b50766d205fcceed069d3
Bilal <bq1214@gmail.com>
2024-11-24 16:26:47 +0000

    feat: add nav links

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,17 +1,51 @@
 <script>
-	import { Plane } from 'lucide-svelte';
+	import { Home, Rocket, Plane, Radar, Info } from 'lucide-svelte';
 </script>
 
 <div class="navbar bg-transparent">
 	<div class="flex-1">KhaleejVACC</div>
 	<div class="flex-none">
 		<ul class="menu menu-horizontal gap-1 px-1">
-			<li><a class="hover:bg-accent hover:bg-opacity-50" href="/">Home</a></li>
+			<li><a class="hover:bg-accent hover:bg-opacity-50" href="/"><Home size={20} /> Home</a></li>
+            <li>
+				<details>
+					<summary class="hover:bg-accent hover:bg-opacity-50"><Rocket size={20} /> Getting Started</summary>
+					<ul class="menu menu-vertical bg-accent bg-opacity-50">
+						<li><a href="/" class="hover:bg-accent" >New to VATSIM</a></li>
+                        <li><a href="/" class="hover:bg-accent">Join Khaleej</a></li>
+                        <li><a href="/" class="hover:bg-accent">ATC Training</a></li>
+					</ul>
+				</details>
+			</li>
 			<li>
 				<details>
 					<summary class="hover:bg-accent hover:bg-opacity-50"><Plane size={20} /> Pilots</summary>
 					<ul class="menu menu-vertical bg-accent bg-opacity-50">
-						<li><a class="hover:bg-accent" href="/">Home</a></li>
+						<li><a href="/" class="hover:bg-accent">Airports & Charts</a></li>
+                        <li><a href="/" class="hover:bg-accent">Events</a></li>
+                        <li><a href="/" class="hover:bg-accent">Event Booking</a></li>
+                        <li><a href="/" class="hover:bg-accent">ATC Feedback</a></li>
+					</ul>
+				</details>
+			</li>
+            <li>
+				<details>
+					<summary class="hover:bg-accent hover:bg-opacity-50"><Radar size={20} /> Controllers</summary>
+					<ul class="menu menu-vertical bg-accent bg-opacity-50">
+						<li><a href="/" class="hover:bg-accent">Training</a></li>
+                        <li><a href="/" class="hover:bg-accent">Roster</a></li>
+                        <li><a href="/" class="hover:bg-accent">Documentation</a></li>
+					</ul>
+				</details>
+			</li>
+            <li>
+				<details>
+					<summary class="hover:bg-accent hover:bg-opacity-50"><Info size={20} /> About</summary>
+					<ul class="menu menu-vertical bg-accent bg-opacity-50">
+						<li><a href="/" class="hover:bg-accent">Staff</a></li>
+                        <li><a href="/" class="hover:bg-accent">Policies</a></li>
+                        <li><a href="/" class="hover:bg-accent">HQ</a></li>
+                        <li><a href="/" class="hover:bg-accent">Contact</a></li>
 					</ul>
 				</details>
 			</li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-    import Header from '$lib/components/Header.svelte';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
 <Header />
 
 <main>Main</main>
 
+<p>Above the divider</p>
+<div class="h-10 bg-gradient-to-t from-gray-500"></div>
+<p class="bg-gray-500">Below the divider</p>
