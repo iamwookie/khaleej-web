da117a40798ad342d99333ae6e3d2502cf1405ae
Wookie <bq1214@gmail.com>
2024-12-21 13:57:37 +0000

    style: header scroll transitions

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import Navbar from './Navbar.svelte';
+
+	let scrollY = $state<number>();
 </script>
 
-<header class="fixed w-full z-10">
+<svelte:window bind:scrollY />
+
+<header class="fixed w-full z-10 transition-all" class:backdrop-blur-lg={scrollY}>
 	<Navbar />
 </header>
