8b42c2b259c8716f1fe877004bd2a9c6bf00a807
Wookie <bq1214@gmail.com>
2024-12-23 14:45:46 +0000

    feat: add initial mobile drawer

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Rocket, Plane, Radar, Info } from 'lucide-svelte';
+	import { Rocket, Plane, Radar, Info, Menu } from 'lucide-svelte';
 
 	const onclick = (e: any) => {
 		for (const details of document.querySelectorAll('details')) {
@@ -10,79 +10,102 @@
 
 <svelte:document {onclick} />
 
-<div class="navbar bg-transparent px-8">
-	<div class="navbar-start">
-		<a href="/" aria-label="Home" class="hover:opacity-50 transition-opacity">
-			<enhanced:img src="../assets/images/logo_khaleej.png" alt="Logo" class="h-32 w-32" />
-		</a>
-	</div>
+<!-- Drawer Wrapper -->
+<div class="drawer drawer-end">
+	<!-- Drawer Toggle -->
+	<input id="mobile-drawer" type="checkbox" class="drawer-toggle" />
 
-	<div class="navbar-center hidden md:inline-flex">
-		<ul class="menu menu-horizontal gap-1 px-1">
-			<li>
-				<details>
-					<summary>
-						<Rocket size={20} /> Getting Started
-					</summary>
+	<div class="drawer-content flex flex-col">
+		<div class="navbar bg-transparent px-8">
+			<div class="navbar-start">
+				<a href="/" aria-label="Home" class="hover:opacity-50 transition-opacity">
+					<enhanced:img src="../assets/images/logo_khaleej.png" alt="Logo" class="h-32 w-32" />
+				</a>
+			</div>
 
-					<ul class="menu menu-vertical bg-secondary bg-opacity-50">
-						<li><a href="/new">New to VATSIM</a></li>
-						<li><a href="/join">Join Khaleej</a></li>
-						<li><a href="/atc/training">ATC Training</a></li>
-					</ul>
-				</details>
-			</li>
+			<div class="navbar-center hidden lg:inline-flex">
+				<ul class="menu menu-horizontal gap-1 px-1">
+					<li>
+						<details>
+							<summary>
+								<Rocket size={20} /> Getting Started
+							</summary>
 
-			<li>
-				<details>
-					<summary>
-						<Plane size={20} /> Pilots
-					</summary>
+							<ul class="menu menu-vertical bg-secondary bg-opacity-50">
+								<li><a href="/new">New to VATSIM</a></li>
+								<li><a href="/join">Join Khaleej</a></li>
+								<li><a href="/atc/training">ATC Training</a></li>
+							</ul>
+						</details>
+					</li>
 
-					<ul class="menu menu-vertical bg-secondary bg-opacity-50 min-w-max">
-						<li><a href="/">Airports & Charts</a></li>
-						<li><a href="/events">Events</a></li>
-						<li><a href="/events/booking">Event Booking</a></li>
-						<li><a href="/atc/feedback">ATC Feedback</a></li>
-					</ul>
-				</details>
-			</li>
+					<li>
+						<details>
+							<summary>
+								<Plane size={20} /> Pilots
+							</summary>
 
-			<li>
-				<details>
-					<summary class="hover:bg-secondary hover:bg-opacity-50">
-						<Radar size={20} /> Controllers
-					</summary>
+							<ul class="menu menu-vertical bg-secondary bg-opacity-50 min-w-max">
+								<li><a href="/">Airports & Charts</a></li>
+								<li><a href="/events">Events</a></li>
+								<li><a href="/events/booking">Event Booking</a></li>
+								<li><a href="/atc/feedback">ATC Feedback</a></li>
+							</ul>
+						</details>
+					</li>
 
-					<ul class="menu menu-vertical bg-secondary bg-opacity-50 min-w-max">
-						<li><a href="/atc/training">Training</a></li>
-						<li><a href="/atc/roster">Roster</a></li>
-						<li><a href="/atc/docs">Documentation</a></li>
-					</ul>
-				</details>
-			</li>
+					<li>
+						<details>
+							<summary class="hover:bg-secondary hover:bg-opacity-50">
+								<Radar size={20} /> Controllers
+							</summary>
 
-			<li>
-				<details>
-					<summary class="hover:bg-secondary hover:bg-opacity-50">
-						<Info size={20} /> About
-					</summary>
+							<ul class="menu menu-vertical bg-secondary bg-opacity-50 min-w-max">
+								<li><a href="/atc/training">Training</a></li>
+								<li><a href="/atc/roster">Roster</a></li>
+								<li><a href="/atc/docs">Documentation</a></li>
+							</ul>
+						</details>
+					</li>
 
-					<ul class="menu menu-vertical bg-secondary bg-opacity-50 min-w-max">
-						<li><a href="/staff">Staff</a></li>
-						<li><a href="/policies">Policies</a></li>
-						<li><a href="/contact">Contact</a></li>
-						<li><a href="/">HQ</a></li>
-					</ul>
-				</details>
-			</li>
-		</ul>
-	</div>
+					<li>
+						<details>
+							<summary class="hover:bg-secondary hover:bg-opacity-50">
+								<Info size={20} /> About
+							</summary>
 
-	<div class="navbar-end hidden md:inline-flex">
-		<div class="inline-flex flex-row">
-			<a href="/join" class="btn btn-outline btn-secondary">Join Now</a>
+							<ul class="menu menu-vertical bg-secondary bg-opacity-50 min-w-max">
+								<li><a href="/staff">Staff</a></li>
+								<li><a href="/policies">Policies</a></li>
+								<li><a href="/contact">Contact</a></li>
+								<li><a href="/">HQ</a></li>
+							</ul>
+						</details>
+					</li>
+				</ul>
+			</div>
+
+			<div class="navbar-end hidden lg:inline-flex">
+				<div class="inline-flex flex-row">
+					<a href="/join" class="btn btn-outline btn-secondary">Join Now</a>
+				</div>
+			</div>
+
+			<div class="navbar-end lg:hidden">
+				<label for="mobile-drawer" class="btn btn-square btn-ghost hover:bg-secondary hover:bg-opacity-50">
+					<Menu size={28} />
+				</label>
+			</div>
 		</div>
+	</div>
+
+	<div class="drawer-side">
+		<label for="mobile-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu bg-base-200 min-h-full w-80 p-4">
+			<!-- Sidebar content here -->
+			<li><a>Sidebar Item 1</a></li>
+			<li><a>Sidebar Item 2</a></li>
+		</ul>
 	</div>
 </div>
 
