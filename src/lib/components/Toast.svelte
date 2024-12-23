b45e57323ec6af0b8cafd6d59ec02e37e2146294
Wookie <bq1214@gmail.com>
2024-12-23 00:59:40 +0000

    feat: add toast visibility

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -1,8 +1,23 @@
-<div class="toast toast-center">
-	<div class="alert bg-base-100">
-		<span
-			>Looking to give ATC feedback? Click <a href="/atc/feedback" class="btn-link text-secondary hover:opacity-50 transition-opacity">here</a
-			>.</span
-		>
+<script lang="ts">
+	import { X } from 'lucide-svelte';
+
+	let visible = $state(false);
+
+	setTimeout(() => (visible = true), 1000);
+</script>
+
+{#if visible}
+	<div class="toast toast-center">
+		<div class="alert bg-base-100">
+			<button class="btn btn-sm" onclick={() => (visible = false)}>
+				<X size={20} />
+			</button>
+
+			<span
+				>Looking to give ATC feedback? Click <a href="/atc/feedback" class="btn-link text-secondary hover:opacity-50 transition-opacity"
+					>here</a
+				>.</span
+			>
+		</div>
 	</div>
-</div>
+{/if}
