19da0885997c9267d192f69e353bda184db3979a
Wookie <bq1214@gmail.com>
2024-12-27 21:25:47 +0000

    fix: meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,13 +11,14 @@
 	<title>Khaleej vACC</title>
 	<meta name="description" content="Khaleej vACC is a virtual area control center for the Bahrain and Kuwait region on the VATSIM network." />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#d8c8a0" />
 
 	<!-- OpenGraph -->
 	<meta name="og:type" content="website" />
 	<meta name="og:title" content="Khaleej vACC" />
 	<meta name="og:description" content="Khaleej vACC is a virtual area control center for the Bahrain and Kuwait region on the VATSIM network." />
-	<meta name="og:image" content="https://khaleejvacc.com/favicon-96x96.png" />
-	<meta name="og:url" content="https://khaleejvacc.com" />
+	<meta name="og:image" content="https://khaleejvacc.net/favicon-96x96.png" />
+	<meta name="og:url" content="https://khaleejvacc.net" />
 </svelte:head>
 
 <Header />
