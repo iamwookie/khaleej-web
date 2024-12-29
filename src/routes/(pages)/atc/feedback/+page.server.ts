e22e9427e1ffb1d793be13ebdd24b7fc42107d2a
Wookie <bq1214@gmail.com>
2024-12-29 23:09:25 +0000

    feat: finish forms

--- src/routes/(pages)/atc/feedback/+page.server.ts
+++ src/routes/(pages)/atc/feedback/+page.server.ts
@@ -1,20 +1,81 @@
 import type { Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+import { fail } from '@sveltejs/kit';
+import { RateLimiter } from 'sveltekit-rate-limiter/server';
+import { superValidate, message } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { z } from 'zod';
+
+import { transporter } from '$lib/server/mailer';
+
+const limiter = new RateLimiter({ IPUA: [1, 'm'] });
+
+const feedbackFormSchema = z.object({
+	pilotName: z.string().min(1, 'Required.'),
+	interactionDate: z.date(),
+	atcName: z.string().min(1, 'Required.'),
+	atcCallsign: z.string().min(1, 'Required.'),
+	rating: z.number().int().min(0).max(100, 'Rating must be between 1 and 5.').default(50),
+	feedback: z.string().min(8, 'Feedback must be at least 8 characters.')
+});
+
+export const load: PageServerLoad = async () => {
+	const form = await superValidate(zod(feedbackFormSchema));
+	return { form };
+};
 
 export const actions: Actions = {
-	default: async ({ request }) => {
-		const data = await request.formData();
+	default: async (event) => {
+		const form = await superValidate(event.request, zod(feedbackFormSchema));
 
-		const pilotName = data.get('pilotName');
-		const interactionDate = data.get('interactionDate');
-		const atcName = data.get('atcName');
-		const atcCallsign = data.get('atcCallsign');
-		const rating = data.get('rating');
-		const feedback = data.get('feedback');
+		if (!form.valid) return fail(400, { form });
 
-		// Add further email logic here, for now we will just log the data
-		console.log({ pilotName, interactionDate, atcName, atcCallsign, rating, feedback });
+		if (await limiter.isLimited(event)) return message(form, { status: 'error', text: 'Too many requests! Try again after a few minutes.' });
 
-		// Return a response to the client, for now we just fail until we add logic
-		return { fail: true };
+		try {
+			const time = new Date();
+
+			const html = `
+		    <!doctype html>
+		    <html lang="en">
+		        <head>
+		            <meta charset="UTF-8" />
+		            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		            <title>ATC Feedback Form Submission</title>
+		        </head>
+		        <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 20px">
+		            <div style="background-color: #ffffff; border-radius: 5px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); padding: 20px; max-width: 600px; margin: auto;">
+		                <h2 style="color: #333">ATC Feedback Form Submission</h2>
+
+		                <p style="color: #555"><strong>Pilot Name:</strong> ${form.data.pilotName}</p>
+		                <p style="color: #555"><strong>Interaction Date:</strong> ${form.data.interactionDate.toLocaleDateString()}</p>
+		                <p style="color: #555"><strong>ATC Name:</strong> ${form.data.atcName}</p>
+                        <p style="color: #555"><strong>ATC Callsign:</strong> ${form.data.atcCallsign}</p>
+                        <p style="color: #555"><strong>Rating (0-100):</strong> ${form.data.rating}</p>
+		                <p style="color: #555"><strong>Feedback:</strong></p>
+		                <p style="color: #555">${form.data.feedback}</p>
+
+		                <div style="margin-top: 20px; font-size: 12px; color: #aaa">
+		                    <p>Submitted on ${time.toLocaleString()}</p>
+		                </div>
+		            </div>
+		        </body>
+		    </html>
+		    `;
+
+			await transporter.sendMail({
+				from: 'noreply@khaleejvacc.net',
+				to: ['atc@khaleejvacc.net', 'hr@khaleejvacc.net'],
+				subject: 'ATC Feedback Form Submission',
+				html
+			});
+
+			return message(form, { status: 'success', text: 'Your feedback has been sent successfully.' });
+		} catch (err) {
+			console.error(err);
+			// More advanced error handling can be added here in the future
+			return message(form, { status: 'error', text: 'Uh oh! An error occured, try again later.' });
+		}
 	}
 };
--- src/routes/(pages)/contact/+page.svelte
+++ src/routes/(pages)/contact/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-	import type { ActionData } from './$types';
+	import { superForm } from 'sveltekit-superforms';
+	import { cn } from '$lib/utils';
 
-	import { enhance } from '$app/forms';
 	import { Mails, CircleCheck, CircleX } from 'lucide-svelte';
-
 	import bgImage from '$lib/assets/images/bg.png';
 
-	let { form } = $props<{ form: ActionData | null }>();
+	let { data } = $props();
+
+	const { form, errors, constraints, message, delayed, enhance } = superForm(data.form);
 </script>
 
 <main class="flex flex-col">
@@ -26,25 +27,28 @@
 
 	<section class="bg-base-300 py-10">
 		<div class="container flex flex-col items-center min-h-[40vh]">
-			{#if form?.success}
-				<div role="alert" class="alert alert-success max-w-md">
-					<CircleCheck size={24} /><span>Your form has been submitted!</span>
-				</div>
-			{/if}
-
-			{#if form?.fail}
-				<div role="alert" class="alert alert-error max-w-md">
-					<CircleX size={24} /><span>Uh oh! Something went wrong!</span>
-				</div>
-			{/if}
-
-			<form method="POST" use:enhance class="card card-body w-full">
+			<form method="POST" class="card card-body w-full" use:enhance>
 				<div class="form-control">
 					<label for="name" class="label">
 						<span class="label-text">Your Name</span>
 					</label>
 
-					<input name="name" type="text" placeholder="Chesley Sullenberger" class="input input-bordered" required />
+					<input
+						type="text"
+						name="name"
+						placeholder="Sully Sullenberger"
+						class={cn('input input-bordered', { 'input-error': $errors.name })}
+						bind:value={$form.name}
+						{...$constraints.name}
+					/>
+
+					{#if $errors.name}
+						<div class="label">
+							<span class="label-text-alt text-error">
+								{$errors.name}
+							</span>
+						</div>
+					{/if}
 				</div>
 
 				<div class="form-control mt-4">
@@ -52,7 +56,22 @@
 						<span class="label-text">Your VATSIM CID</span>
 					</label>
 
-					<input name="vatsimCid" type="number" placeholder="1234567" class="input input-bordered" required />
+					<input
+						type="number"
+						name="vatsimCid"
+						placeholder="1514902"
+						class={cn('input input-bordered', { 'input-error': $errors.vatsimCid })}
+						bind:value={$form.vatsimCid}
+						{...$constraints.vatsimCid}
+					/>
+
+					{#if $errors.vatsimCid}
+						<div class="label">
+							<span class="label-text-alt text-error">
+								{$errors.vatsimCid}
+							</span>
+						</div>
+					{/if}
 				</div>
 
 				<div class="form-control mt-4">
@@ -60,7 +79,22 @@
 						<span class="label-text">Your Email</span>
 					</label>
 
-					<input name="email" type="email" placeholder="example@mail.com" class="input input-bordered" required />
+					<input
+						type="email"
+						name="email"
+						placeholder="example@mail.com"
+						class={cn('input input-bordered', { 'input-error': $errors.email })}
+						bind:value={$form.email}
+						{...$constraints.email}
+					/>
+
+					{#if $errors.email}
+						<div class="label">
+							<span class="label-text-alt text-error">
+								{$errors.email}
+							</span>
+						</div>
+					{/if}
 				</div>
 
 				<div class="form-control mt-4">
@@ -68,15 +102,45 @@
 						<span class="label-text">Message</span>
 					</label>
 
-					<textarea name="message" placeholder="I would like to contact you regarding..." class="textarea textarea-bordered h-24" required
+					<textarea
+						name="message"
+						placeholder="I would like to contact you regarding..."
+						class={cn('textarea textarea-bordered h-24', { 'input-error': $errors.message })}
+						bind:value={$form.message}
+						{...$constraints.message}
 					></textarea>
+
+					{#if $errors.message}
+						<div class="label">
+							<span class="label-text-alt text-error">
+								{$errors.message}
+							</span>
+						</div>
+					{/if}
 				</div>
 
 				<div class="form-control mt-6">
-                    <!-- Disable forms for now -->
-					<button type="submit" class="btn btn-secondary btn-disabled">Submit</button>
+					<button type="submit" class={cn('btn btn-secondary', { 'btn-disabled': $delayed })}>
+						{#if $delayed}
+							<span class="loading loading-spinner"></span>
+						{:else}
+							Submit
+						{/if}
+					</button>
 				</div>
 			</form>
+
+			{#if $message}
+				{#if $message.status == 'success'}
+					<div role="alert" class="alert alert-success max-w-md">
+						<CircleCheck size={24} /><span>{$message.text}</span>
+					</div>
+				{:else if $message.status == 'error'}
+					<div role="alert" class="alert alert-error max-w-md">
+						<CircleX size={24} /><span>{$message.text}</span>
+					</div>
+				{/if}
+			{/if}
 		</div>
 	</section>
 </main>
